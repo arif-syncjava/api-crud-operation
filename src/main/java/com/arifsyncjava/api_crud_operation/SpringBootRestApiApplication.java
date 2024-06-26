2533e7d3017f23f5b7ddb14178afa81883cf3cd5
MD ARIFUL ISLAM <arif.syncjava@gmail.com>
2024-06-26 21:47:30 +0600

    project setup with initial commit

---
+++ src/main/java/com/arifsyncjava/api_crud_operation/SpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.arifsyncjava.api_crud_operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiApplication.class, args);
+	}
+
+}
